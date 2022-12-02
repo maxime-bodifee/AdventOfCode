17f44f7d22be8b86edb67ed8e1271ce7e67bf947
maxime-bodifee <max.bodifee@gmail.com>
2022-12-02 00:35:19 -0800

    Refactored 'score' to 'score_total'

--- day_2/rock_paper_scissors.py
+++ day_2/rock_paper_scissors.py
@@ -11,30 +11,30 @@ def solution_1():
             self.me = me
 
         def score(self):
-            score = 0
+            score_total = 0
 
             match self.me:
                 case "X":
-                    score += 1
+                    score_total += 1
 
                 case "Y":
-                    score += 2
+                    score_total += 2
 
                 case "Z":
-                    score += 3
+                    score_total += 3
 
             if (self.elf == "A" and self.me == "Y") or (self.elf == "B" and self.me == "Z") or (
                     self.elf == "C" and self.me == "X"):
-                score += 6
+                score_total += 6
 
             elif (self.elf == "A" and self.me == "X") or (self.elf == "B" and self.me == "Y") or (
                     self.elf == "C" and self.me == "Z"):
-                score += 3
+                score_total += 3
 
             else:
-                score += 0
+                score_total += 0
 
-            return score
+            return score_total
 
     score = 0
 
@@ -52,46 +52,46 @@ def solution_2():
             self.outcome = outcome
 
         def score(self):
-            score = 0
+            score_total = 0
 
             match self.outcome:
                 case "X":
-                    score += 0
+                    score_total += 0
 
                     if self.elf == "A":
-                        score += 3
+                        score_total += 3
 
                     elif self.elf == "B":
-                        score += 1
+                        score_total += 1
 
                     elif self.elf == "C":
-                        score += 2
+                        score_total += 2
 
                 case "Y":
-                    score += 3
+                    score_total += 3
 
                     if self.elf == "A":
-                        score += 1
+                        score_total += 1
 
                     elif self.elf == "B":
-                        score += 2
+                        score_total += 2
 
                     elif self.elf == "C":
-                        score += 3
+                        score_total += 3
 
                 case "Z":
-                    score += 6
+                    score_total += 6
 
                     if self.elf == "A":
-                        score += 2
+                        score_total += 2
 
                     elif self.elf == "B":
-                        score += 3
+                        score_total += 3
 
                     elif self.elf == "C":
-                        score += 1
+                        score_total += 1
 
-            return score
+            return score_total
 
     score = 0
 
