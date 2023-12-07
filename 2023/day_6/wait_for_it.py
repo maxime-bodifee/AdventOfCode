d5ccf180ab454af182db741765cbce7d42184780
Max <max.bodifee@gmail.com>
2023-12-07 13:59:27 -0800

    Fixed Condition For O(n) Solution For Day 6 2023

--- 2023/day_6/wait_for_it.py
+++ 2023/day_6/wait_for_it.py
@@ -6,7 +6,7 @@ with open("input.txt") as file:
 t1 = list(map(int, times.strip("Time: ").split()))
 d1 = list(map(int, distances.strip("Distance: ").split()))
 
-print(prod(sum(i * (t - i) > d for i in range(t + 1)) for t, d in zip(t1, d1)))
+print(prod(sum(i * (t - i) >= d for i in range(t + 1)) for t, d in zip(t1, d1)))
 
 t2 = int(times.strip("Time:").replace(" ", ""))
 d2 = int(distances.strip("Distance:").replace(" ", ""))
@@ -16,4 +16,4 @@ n = (t2 ** 2 - d2 * 4) ** 0.5
 print(round(n - 0.5) if n % 1 >= 0.5 else round(n + 0.5))
 
 # O(n)
-print(sum(i * (t2 - i) > d2 for i in range(t2 + 1)))
+print(sum(i * (t2 - i) >= d2 for i in range(t2 + 1)))
