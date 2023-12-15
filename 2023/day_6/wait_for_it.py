c6a757b994bc80ca7e0299a3368c79a957d65016
maxime-bodifee <max.bodifee@gmail.com>
2023-12-15 03:51:22 -0800

    Fixed Day 6 2023

--- 2023/day_6/wait_for_it.py
+++ 2023/day_6/wait_for_it.py
@@ -6,7 +6,7 @@ with open("input.txt") as file:
 t1 = list(map(int, times.strip("Time: ").split()))
 d1 = list(map(int, distances.strip("Distance: ").split()))
 
-print(prod(sum(i * (t - i) >= d for i in range(t + 1)) for t, d in zip(t1, d1)))
+print(prod(sum(i * (t - i) > d for i in range(t + 1)) for t, d in zip(t1, d1)))
 
 t2 = int(times.strip("Time:").replace(" ", ""))
 d2 = int(distances.strip("Distance:").replace(" ", ""))
@@ -17,4 +17,4 @@ x1, x2 = (t2 + det) / 2, (t2 - det) / 2
 print(ceil(x1) - ceil(x2) + int(x1.is_integer() and x2.is_integer()))
 
 # O(n)
-print(sum(i * (t2 - i) >= d2 for i in range(t2 + 1)))
+print(sum(i * (t2 - i) > d2 for i in range(t2 + 1)))
