b3ab69974977764c0d62843dc76812a247e097ac
Max Bodifee <max_bodifee@sfu.ca>
2024-12-02 21:51:11 -0800

    Solved Day 3 2024

---
+++ 2024/day_3/mull_it_over.py
@@ -0,0 +1,33 @@
+import re
+
+with open("input.txt") as file:
+    inp = file.read()
+
+total_1 = 0
+
+for row in re.findall(r"mul\(\d+,\d+\)", inp):
+    a, b = re.search(r"(\d+),(\d+)", row).groups()
+    total_1 += int(a) * int(b)
+
+print(total_1)
+
+# alternative: part 1 in a single line
+print(sum(int(a) * int(b) for mul in re.findall(r"mul\(\d+,\d+\)", inp) for a, b in [re.search(r"(\d+),(\d+)", mul).groups()]))
+
+total_2 = 0
+enabled = True
+for row in re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", inp):
+    if row == "do()":
+        enabled = True
+        continue
+    elif row == "don't()":
+        enabled = False
+        continue
+
+    if not enabled:
+        continue
+
+    a, b = re.search(r"(\d+),(\d+)", row).groups()
+    total_2 += int(a) * int(b)
+
+print(total_2)
